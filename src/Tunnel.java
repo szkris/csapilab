6872c3cd30e9fdc4650a9336f8d57edb52bfe6a9
DESKTOP-EECSIA8\szabo <szaboo.kris@gmail.com>
2017-04-15 14:27:46 +0200

    Tunnel osztály megvalósítása #1.

--- src/Tunnel.java
+++ src/Tunnel.java
@@ -1,33 +1,27 @@
-
 import java.util.*;
 
 /**
- * 
+ * Egy alagutat megvalósító osztály.
  */
 public class Tunnel extends Rail {
+	
+    /**
+     *  Itt tároljuk, hogy az alagút melyik bejáratokhoz vezethet.
+     */
+    private List<TunnelEntrance> entrances;
 
     /**
-     * Default constructor
+     * Default konstruktor.
      */
     public Tunnel() {
+    	entrances = new ArrayList<TunnelEntrance>();
     }
 
     /**
-     * 
+     * @param entrance Az adott kijárat, amirõl meg akarjuk határozni, hogy oda vezet-e az alagút.
+     * @return  Megadja hogy az adott alagútrész vezet-e a kapott kijárathoz.
      */
-    private Table table;
-
-    /**
-     * 
-     */
-    private TableElement leadsto;
-
-    /**
-     * @param TunnelEntrance 
-     * @return
-     */
-    public boolean leadsTo(TunnelEntrance te) {
-        // TODO implement here
-        return false;
+    public boolean leadsTo(TunnelEntrance entrance) {
+    	return entrances.contains(entrance);
     }
 }
