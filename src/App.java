94a04bab747b3f7eef7642da1d0b5d3e906bf53e
Dani <nyitraida@gmail.com>
2017-04-17 16:34:36 +0200

    while(true)

--- src/App.java
+++ src/App.java
@@ -22,41 +22,42 @@ public class App {
 		System.out.println("parancsok:");
         String line;
 		line = br.readLine();
-        String[] s = line.split(" ");
-        if(s[0].equals("loadMap")){
-        	Game.loadMap("s[1]");
-        }
-        else if(s[0].equals("startTrain")){
-        	
-        }
-        else if(s[0].equals("setSwitch")){
-        	
-        }
-        else if(s[0].equals("openTunnelEntrance")){
-        	
-        }
-        else if(s[0].equals("closeTunnelEntrance")){
-        	
-        }
-        else if(s[0].equals("step")){
-        	
-        }
-        else if(s[0].equals("listStations")){
-        	
-        }
-        else if(s[0].equals("listSwitches")){
-        	
-        }
-        else if(s[0].equals("listTunnelEntrance")){
-        	
-        }
-        else if(s[0].equals("listTrain")){
-        	
-        }
-        else if(s[0].equals("hopOn")){
-        	
-        }
-		game.loadMap("tunnelentrance_test_map.xml");
+		while(true){
+			String[] s = line.split(" ");
+	        if(s[0].equals("loadMap")){
+	        	Game.loadMap("s[1]");
+	        }
+	        else if(s[0].equals("startTrain")){
+	        	
+	        }
+	        else if(s[0].equals("setSwitch")){
+	        	
+	        }
+	        else if(s[0].equals("openTunnelEntrance")){
+	        	
+	        }
+	        else if(s[0].equals("closeTunnelEntrance")){
+	        	
+	        }
+	        else if(s[0].equals("step")){
+	        	
+	        }
+	        else if(s[0].equals("listStations")){
+	        	
+	        }
+	        else if(s[0].equals("listSwitches")){
+	        	
+	        }
+	        else if(s[0].equals("listTunnelEntrance")){
+	        	
+	        }
+	        else if(s[0].equals("listTrain")){
+	        	
+	        }
+	        else if(s[0].equals("hopOn")){
+	        	
+	        }
+		}
 		game.run();
 	}
 
