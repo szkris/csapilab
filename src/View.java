dbc64b18bfc3cc639290f31d0bcf2e7f966d280e
Dani <nyitraida@gmail.com>
2017-05-06 20:01:15 +0200

    View

---
+++ src/View.java
@@ -0,0 +1,23 @@
+import javax.swing.JPanel;
+
+public class View {
+	protected JPanel panel;
+	
+	private Table tableCopy;
+	
+	public int loadMap(){
+		
+	}
+	
+	public void drawAll(){
+		
+	}
+	
+	public void addTrain(Train t){
+		
+	}
+	
+	public void click(int x, int y){
+		
+	}
+}
