9f72a83562fa64af0e5aec0494e9fd9b3d91ebcd
Dani <nyitraida@gmail.com>
2017-05-07 14:12:10 +0200

    Menu panel

--- src/Menu.java
+++ src/Menu.java
@@ -1,3 +1,8 @@
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class Menu {
@@ -5,8 +10,30 @@ public class Menu {
 	
 	public Menu(){
 		panel = new JPanel();
+		panel.setBackground(Color.RED);
+		//Buttons
+			JButton newGameButton = new JButton("Új játék");
+			JButton exitButton = new JButton("Kilépés");
+			newGameButton.addActionListener(new newGameButtonActionListener());
+			exitButton.addActionListener(new exitButtonActionListener());
+			panel.add(newGameButton);
+			panel.add(exitButton);
 	}
 	
+	public class newGameButtonActionListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			newGame();
+		}
+	}
+
+	public class exitButtonActionListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			exit();
+		}
+	}
+
 	public void newGame(){
 		
 	}
